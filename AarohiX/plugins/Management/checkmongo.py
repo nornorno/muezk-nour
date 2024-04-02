8a6e8b1d2f7c5afb8f25ff3b1a0fec58cf44fc9b
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-02 03:05:00 +0200

    Create checkmongo.py

---
+++ AarohiX/plugins/Management/checkmongo.py
@@ -0,0 +1,26 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pymongo import MongoClient
+import re
+from AarohiX  import app as bot
+
+mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
+
+
+@bot.on_message(filters.command(["فحص المنجو"], prefixes=[""])
+async def mongo_command(client, message: Message):
+    if len(message.command) < 2:
+        await message.reply("Please enter your MongoDB URL after the command. Example: /mongochk your_mongodb_url")
+        return
+
+    mongo_url = message.command[1]
+    if re.match(mongo_url_pattern, mongo_url):
+        try:
+            # Attempt to connect to the MongoDB instance
+            client = MongoClient(mongo_url, serverSelectionTimeoutMS=5000)
+            client.server_info()  # Will cause an exception if connection fails
+            await message.reply("𝗠𝗼𝗻𝗴𝗼𝗗𝗕 𝗨𝗥𝗟 𝗶𝘀 𝘃𝗮𝗹𝗶𝗱 𝗮𝗻𝗱 𝗰𝗼𝗻𝗻𝗲𝗰𝘁𝗶𝗼𝗻 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹✅")
+        except Exception as e:
+            await message.reply(f"Failed to connect to MongoDB: {e}")
+    else:
+        await message.reply("𝗜𝗻𝘃𝗮𝗹𝗶𝗱 𝗠𝗼𝗻𝗴𝗼𝗗𝗕 𝗨𝗥𝗟 𝗳𝗼𝗿𝗺𝗮𝘁💔")
