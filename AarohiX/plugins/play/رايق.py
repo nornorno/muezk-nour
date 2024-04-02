cfc42c7d6bf28a5111666e0375b6f02f5577d5f7
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-02 06:15:19 +0200

    Update رايق.py

--- AarohiX/plugins/play/رايق.py
+++ AarohiX/plugins/play/رايق.py
@@ -13,12 +13,15 @@ from AarohiX import app
 from random import  choice, randint
 
 
-@app.on_message(filters.command(["رايك بصورتي"], ""))
+@app.on_message(filters.command(["جمالي"], ""))
 async def madison(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.get_chat_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""صورتك 🐉""", 
+                    await message.reply_photo(photo.file_id,       caption=f"""**⭓ᴍᴜꜱɪᴄ✘ᴛᴇᴛᴏ♪
+╮⦿ هل خلق الجمال لتختصره عيناك اما
+│᚜⦿ عيناك خلقت لتقنعني ان لا جمال بعدها
+╯⦿ جمال عيناك (60) % ❤️**""", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
