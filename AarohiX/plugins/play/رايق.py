d49e262968adb3dcc2626202a1c29fcce4791768
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-02 06:06:09 +0200

    Update رايق.py

--- AarohiX/plugins/play/رايق.py
+++ AarohiX/plugins/play/رايق.py
@@ -1,18 +1,24 @@
 import asyncio
-from pyrogram import Client, filters
-from strings import get_string
-from strings.filters import command
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup
-from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
-RAEAK = ["فاجره","حلوه","فخامه","جميله","خوش","جميله","يععععع","وحشه","مش حلوه","حلوه ياعم","خليك بيها","حبتها","غيرها يعم"]
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+
+from AarohiX import app
+from random import  choice, randint
+
 
 @app.on_message(filters.command(["رايك بصورتي"], ""))
 async def madison(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.get_chat_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""صورتك {choice(RAEAK)} 🐉""", 
+                    await message.reply_photo(photo.file_id,       caption=f"""صورتك 🐉""", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
