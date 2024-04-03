8efdd70f98072e5751c68ea1ab6e9050e8b86ba2
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-03 22:06:29 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -1,7 +1,16 @@
-from pyrogram import Client
-from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
-from AarohiX import app
+import asyncio
 import os
+import time
+import requests
+import aiohttp
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AarohiX import app
+from asyncio import gather
+from pyrogram.errors import FloodWait
 
 @app.on_message(filters.command(["مح"], ""))
 async def maker(client: Client, message: Message):
