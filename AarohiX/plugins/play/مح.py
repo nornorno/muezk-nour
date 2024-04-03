513febc8f9884e202661cefc14bc4d2c037813a1
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-03 22:04:20 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -1,16 +1,7 @@
-import asyncio
-
-import os
-import time
-import requests
-from config import START_IMG_URL
-from pyrogram import filters
-import random
 from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-
+from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
 from AarohiX import app
-from random import  choice, randint
+import os
 
 @app.on_message(filters.command(["مح"], ""))
 async def maker(client: Client, message: Message):
