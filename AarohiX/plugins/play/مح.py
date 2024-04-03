8c2f72af99fb251141e4357f4b542b7e2aa0d653
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-03 20:52:22 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -5,12 +5,11 @@ from AarohiX import app
 import config
 
 
-@app.on_message(
-    command(["مح"])
+@app.on_message(filters.command("مح") &  filters.private & SUDOERS)
 )
 async def maker(client: Client, message: Message):
-    await message.reply_photo(
-        photo="https://telegra.ph/file/dd1298fa95a28d6962705.jpg",
+    await message.reply_video(
+        video="https://telegra.ph/file/dd1298fa95a28d6962705.jpg",
         caption="※ هذا القميل {message.from_user.mention} \n※ بعتلك بوسه يا 😘♥ [{user.first_name}](tg://user?id={user.id})\n عيب كده اي المحن ده 😹",
         reply_markup=InlineKeyboardMarkup(
             [
