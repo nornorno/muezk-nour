910508bf0df2c25ee0211bfd341a2dcdd464fe1a
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-04 04:20:33 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -1,9 +1,9 @@
-import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
 from AarohiX import app
 import config
+from pyrogram.errors import FloodWait
 
 
 
@@ -12,10 +12,7 @@ async def maker(client: Client, message: Message):
      await message.reply_video(
         video="https://telegra.ph/file/83e7bdf0e2dad83402160.mp4",
         caption="•القميل هذا ✨♥ \n※ بعتلك بوسه يا 😘♥ @ \nعيب كده اي المحن ده 😹",
-        reply_markup=InlineKeyboardMarkup(
-            [
-            [
-                    reply_markup=InlineKeyboardMarkup(
+            reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
