bec24de10d66bf20cab7b8512f32551d8e2b22c0
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-04 18:14:40 +0200

    Update gban.py

--- AarohiX/plugins/sudo/gban.py
+++ AarohiX/plugins/sudo/gban.py
@@ -20,7 +20,7 @@ from AarohiX.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["حظر عام"], "")) & SUDOERS)
+@app.on_message(filters.command(["حظر عام", "حظر ع"]) & SUDOERS)
 @language
 async def global_ban(client, message: Message, _):
     if not message.reply_to_message:
