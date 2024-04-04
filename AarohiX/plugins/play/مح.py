f7b145ed2a28753a17f3fcd904dc7be9a54c302b
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-04 04:31:26 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -11,17 +11,11 @@ from pyrogram.errors import FloodWait
 async def maker(client: Client, message: Message):
      await message.reply_video(
         video="https://telegra.ph/file/83e7bdf0e2dad83402160.mp4",
-        caption="•القميل هذا ✨♥ \n※ بعتلك بوسه يا 😘♥ @ \nعيب كده اي المحن ده 😹",
+        caption="•القميل هذا ✨♥ https://t.me/{message.from_user.username} \n※ بعتلك بوسه يا 😘♥ @ \nعيب كده اي المحن ده 😹",
             reply_markup=InlineKeyboardMarkup(
             [
                 [
-                   InlineKeyboardButton(
-                        user.mention,
-                        "المقبول 🫧", url=f"https://t.me/{user.mention}"
-                    ),
-                ],
-                [
-                     InlineKeyboardButton(
+                    InlineKeyboardButton(
                         message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
                 ],
             ]
