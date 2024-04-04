5903d6a694cdc0fe7a9e216143fc7c6244871ab1
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-04 04:55:26 +0200

    Update مح.py

--- AarohiX/plugins/play/مح.py
+++ AarohiX/plugins/play/مح.py
@@ -7,10 +7,8 @@ from pyrogram.errors import FloodWait
 
 
 
-@app.on_message(command("مح"))
-async def tnzelnmla(client:Client, message:Message):
-  if message.reply_to_message.from_user.mention in zog:
-    zog.remove(message.reply_to_message.from_user.mention)
+@app.on_message(filters.command(["مح"], ""))
+async def maker(client: Client, message: Message):
      await message.reply_video(
         video="https://telegra.ph/file/83e7bdf0e2dad83402160.mp4",
         caption="•القميل هذا ✨♥ https://t.me/{message.from_user.username} \n※ بعتلك بوسه يا 😘♥ @ \nعيب كده اي المحن ده 😹",
