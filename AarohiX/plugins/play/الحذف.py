4fa956909fd16487162877fb140156d82623cff8
احـمد تيـتو <165491833+DevAhmedTeto@users.noreply.github.com>
2024-04-04 07:52:58 +0200

    Create الحذف.py

---
+++ AarohiX/plugins/play/الحذف.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from strings.filters import command
+from AarohiX import app
+import config
+from pyrogram.errors import FloodWait
+
+
+
+
+@app.on_message(filters.command(["رابط الحذف"], ""))
+async def maker(client: Client, message: Message):
+     await message.reply_video(
+        video="https://telegra.ph/file/7a7e4909d7f78e8d4c685.mp4",
+        caption="- مـع السلامه بيبي 🫧[اطغط هنا](https://t.me/LC6BOT)",
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                     InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
