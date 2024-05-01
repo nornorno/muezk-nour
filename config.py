67b25564bcada893cad7dde3b9d70c8b759844bf
ᴀHᴍᴇᴅ Tᴇᴛᴏ <157909403+DevTeto@users.noreply.github.com>
2024-05-01 21:23:42 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DevAhmedTeto/TetoMusic",
+    "https://github.com/noooortog/Cr-noor",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
