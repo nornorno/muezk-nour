2c11dbf55b7eab12fbe56b5a48e178ec141d6099
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-03 01:26:56 +0300

    Update Sourceteto.py

--- AarohiX/plugins/play/Sourceteto.py
+++ AarohiX/plugins/play/Sourceteto.py
@@ -20,19 +20,19 @@ async def huhh(client: Client, message: Message):
     await message.reply_video(
         video=f"https://graph.org/file/862ce6e007a09bfd9d2a8.mp4",
         caption=f"""˛ ╭──── • ◈ • ────╮
-么 [𝗦𝗼𝘂𝗿𝗰𝗲 𝗖𝗥](https://t.me/vzo_a)
-么 [𝗔𝗦𝗞 𝗧𝗢 𝗠𝗘](https://t.me/SQ_RM)
-么 [•𓏺 َِ𝘾َِ𝘳- َِ𝙉َِ𝘰َِ𝙐َِ𝘳](https://t.me/j_d_z)
+么 [𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/vzo_a)
+么 [𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/cr_eo)
+么 [𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/j_d_z)
 ╰──── • ◈ • ────╯
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 .""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Cr NooR 🖤", url=f"https://t.me/j_d_z"), 
+                        "𝙲𝚁 𝙽𝙾𝚄𝚁", url=f"https://t.me/j_d_z"), 
                  ],[
                    InlineKeyboardButton(
-                        "CH SOURCE", url=f"https://t.me/vzo_a"),
+                        "𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁", url=f"https://t.me/vzo_a"),
                 ],
 
             ]
