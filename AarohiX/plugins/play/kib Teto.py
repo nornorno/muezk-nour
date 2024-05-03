ca51e770c2af816a86f56fda8879bf905319206c
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-04 02:51:10 +0300

    Update kib Teto.py

--- AarohiX/plugins/play/kib Teto.py
+++ AarohiX/plugins/play/kib Teto.py
@@ -80,7 +80,7 @@ def reply_to_HEY(Client, message):
         reply_markup=InlineKeyboardMarkup(
             [
             [
-                InlineKeyboardButton("ᯓ سورس ميوزك تيتو", url=f"https://t.me/WX_PM"),
+                InlineKeyboardButton("ᯓ سورس ميوزك نور الحاكم", url=f"https://t.me/vzo_a"),
             ]
          ]
      )
