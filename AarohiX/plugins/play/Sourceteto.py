e3161307a51fa20f129e3f169a06e15e3433aad6
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-12 19:01:48 +0300

    Update Sourceteto.py

--- AarohiX/plugins/play/Sourceteto.py
+++ AarohiX/plugins/play/Sourceteto.py
@@ -25,12 +25,12 @@ async def huhh(client: Client, message: Message):
 么 [𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/nor_o)
 ╰──── • ◈ • ────╯
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 
-         😜سورس نور الحاكم😜  
-
+     😜سورس نور الحاكم😜   
 • My Official Channel 💚
 • I post here Source & New Updates 💖
 • My official account Dev¹ : @nor_o  🖤
 • My official account Dev² : @O_F_4 🤍
+• My official account Dev² : @KA_5N 🧐
 • My bot : https://t.me/cr_co_bot 💛
 .""",
         reply_markup=InlineKeyboardMarkup(
