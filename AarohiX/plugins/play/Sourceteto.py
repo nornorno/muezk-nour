81de3502ff03c14e71a9208bf161564e2265dd82
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-12 18:49:13 +0300

    Update Sourceteto.py

--- AarohiX/plugins/play/Sourceteto.py
+++ AarohiX/plugins/play/Sourceteto.py
@@ -15,7 +15,7 @@ from random import  choice, randint
 
 #writing by teto @G_7_Rr          
                 
-@app.on_message(filters.command(["السورس","ياسورس","مبرمج السورس","مطور السورس"],""))
+@app.on_message(filters.command(["السورس","نورسورس","مبرمج السورس","مطور السورس"],""))
 async def huhh(client: Client, message: Message):
     await message.reply_video(
         video=f"https://graph.org/file/862ce6e007a09bfd9d2a8.mp4",
@@ -24,8 +24,13 @@ async def huhh(client: Client, message: Message):
 么 [𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/cr_eo)
 么 [𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/nor_o)
 ╰──── • ◈ • ────╯
-قناه سورس الميوزك https://t.me/vzo_a
-⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 .""",
+⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 
+ط• My Official Channel 💚
+• I post here Source & New Updates 💖
+• My official account Dev¹ : @nor_o  🖤
+• My official account Dev² : @O_F_4 🤍
+• My bot : https://t.me/cr_co_bot 💛
+.""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
