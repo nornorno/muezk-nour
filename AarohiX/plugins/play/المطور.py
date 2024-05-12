000d348a439f39480269925e329f30db7c32ab3a
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-12 19:32:28 +0300

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -11,15 +11,7 @@ import config
 async def maker(client: Client, message: Message):
     await message.reply_photo(
         photo="https://telegra.ph/file/3955f6d7c023440c11156.jpg",
-        caption="• Dev Bot ↦ المطور \n ━━━━━━━━━━━━ \n • Dev ↦  Cr SoUrce . \n • Bio ↦ ˛ ╭──── • ◈ • ────╮
-
-么• My Official Channel 💚
-么• I post here Source & New Updates 💖
-么• My official account Dev¹ : @nor_o  🖤
-么• My official account Dev² : @O_F_4 🤍
-
-╰──── • ◈ • ────╯
-.",
+        caption="• Dev Bot ↦ المطور نور الحاكم \n ━━━━━━━━━━━━ \n • Dev ↦ { nor_o.t.me }{ o_f_4.t.me } Cr SoUrce . \n • Bio ↦𝗘𝗩𝗘 #𝗥𝗬𝗧𝗛𝗜𝗡𝗚 𝗧𝗛𝗜𝗦 #𝗔𝗖𝗖𝗢𝗨𝗡𝗧 { noordot.t.me } { @O_F_4 }{ sahnks.t.me }{ vza_o.t.me } { h_n_y_o.t.me }{ vzo_a.t.me }{ mssna_1bot.t.me }{ noonelhakem_123bot.t.me }{ cr_co_bot.t.me }",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
