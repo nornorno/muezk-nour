22f3bb6c98f54e92c30ea5dd26744637bf473204
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-13 19:44:47 +0300

    Update Sourceteto.py

--- AarohiX/plugins/play/Sourceteto.py
+++ AarohiX/plugins/play/Sourceteto.py
@@ -20,18 +20,17 @@ async def huhh(client: Client, message: Message):
     await message.reply_video(
         video=f"https://graph.org/file/862ce6e007a09bfd9d2a8.mp4",
         caption=f"""˛ ╭──── • ◈ • ────╮
-么 [𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/xx6uxx)
-么 [𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/XIXXIXl1l)
-么 [𝙲𝚁 𝙽𝙾𝚄𝚁](https://t.me/X_II_H)
+么 [𝚂𝙾𝚄𝚁𝙲𝙴:αℓτυяĸy](https://t.me/xx6uxx)
+么 [𝚂𝙾𝚄𝚁𝙲𝙴:αℓτυяĸy](https://t.me/XIXXIXl1l)
+么 [αℓτυяĸy](https://t.me/X_II_H)
 ╰──── • ◈ • ────╯
 ⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼 
 • My Official Channel 💚
-• I post here Source & New Updates 💖
-• My official account Dev¹ : @X_II_H  🖤
-• My official account Dev² : @XxXxXl1l 🤍
-• My official account Dev² : @U_II_H 🧐
-• My bot : https://t.me/x_ZlKA_x_bot 💛
-.
+• I post here Source & New Updates
+• My official account Dev¹ : @X_II_H 
+• My official account Dev² : @XxXxXl1l
+• My official account Dev² : @U_II_H
+• My bot : https://t.me/x_ZlKA_x_bot
 .""",
         reply_markup=InlineKeyboardMarkup(
             [
