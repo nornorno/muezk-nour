304418dff1afd59ac4af488804cb9a5c71130a61
noooortog <168671984+noooortog@users.noreply.github.com>
2024-05-13 16:55:36 +0300

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -6,17 +6,17 @@ import config
 
 
 @app.on_message(
-    command(["مطور", "مبرمج السورس", "نور"])
+    command(["مطور", "مبرمج السورس", "زيكا"])
 )
 async def maker(client: Client, message: Message):
     await message.reply_photo(
-        photo="https://te.legra.ph/file/b39f0cd77aa8fcba21b5d.jpg",
-        caption="• Dev Bot ↦ المطور نور الحاكم \n ━━━━━━━━━━━━ \n • Dev ↦ { nor_o.t.me } Cr SoUrce . \n • Bio ↦𝗘𝗩𝗘 #𝗥𝗬𝗧𝗛𝗜𝗡𝗚 𝗧𝗛𝗜𝗦 #𝗔𝗖𝗖𝗢𝗨𝗡𝗧 { noordot.t.me } { @O_F_4 }{ sahnks.t.me }{ vza_o.t.me } { h_n_y_o.t.me }{ vzo_a.t.me }{ mssna_1bot.t.me }{ noonelhakem_123bot.t.me }{ cr_co_bot.t.me }",
+        photo="https://te.legra.ph/file/77bd924e46880da2b8d38.jpg",
+        caption="• Dev Bot ↦ المطور السورس زيكا و اسلام \n ━━━━━━━━━━━━ \n • Dev ↦  Cr SoUrce . \n • Bio ↦صلي علي سيدنا محمد عليه الصلاه والسلام 💕",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𝙲𝚁 𝙽𝙾𝚄𝚁", url=f"https://t.me/nor_o"
+                        "αℓτυяĸy", url=f"https://t.me/X_II_H"
                     ),
                 ],
                 [
