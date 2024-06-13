734d1cf29b1f1b349c56b99643c95a4b115d48f0
nornorno <noureteo@gmail.com>
2024-06-13 17:30:24 +0300

    Update الحذف.py

--- AarohiX/plugins/play/الحذف.py
+++ AarohiX/plugins/play/الحذف.py
@@ -11,7 +11,7 @@ from pyrogram.errors import FloodWait
 @app.on_message(filters.command(["بوت حذف","رابط الحذف","حذف حساب","عاوز احذف"], ""))
 async def maker(client: Client, message: Message):
      await message.reply_video(
-        video="https://telegra.ph/file/7a7e4909d7f78e8d4c685.mp4",
+        video="https://telegra.ph/file/06b0d6f4227097d519aa8.mp4",
         caption="◍ مع السـلامه اراك لاحقـا 🥀 ❲ [اطغط هنا](https://t.me/LC6BOT) ❳",
             reply_markup=InlineKeyboardMarkup(
             [
